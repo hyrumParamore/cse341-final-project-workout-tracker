617936e16c8fbe7e594c95075dc8e3b9d2bf19f0
Hyrum Paramore <par17057@byui.edu>
2024-03-12 12:57:43 -0600

    changing a few thigns to try

--- model/userModel.js
+++ model/userModel.js
@@ -3,9 +3,7 @@ const { getDb } = require('../db/connect');
 const createUser = async (userData) => {
     const db = getDb();
     try {
-        const result = await db.collection('users').insertOne(userData);
-        console.log("User created:", result.ops[0]);
-        return result.ops[0];
+        return await db.collection('users').insertOne(userData);
     } catch (err) {
         console.error('Error creating user:', err);
         throw err;
@@ -14,14 +12,7 @@ const createUser = async (userData) => {
 
 const findUserByGoogleEmail = async (email) => {
     const db = getDb();
-    try {
-        const user = await db.collection('users').findOne({ email });
-        console.log("Found user:", user);
-        return user;
-    } catch (err) {
-        console.error('Error finding user by email:', err);
-        throw err;
-    }
+    return await db.collection('users').findOne({ email });
 };
 
 module.exports = {
