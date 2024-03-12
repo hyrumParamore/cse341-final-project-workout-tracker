3ce112e71f2a5ec3d52460268fa38b5aeb76f261
Hyrum Paramore <par17057@byui.edu>
2024-03-12 12:00:44 -0600

    trying to fix the create user

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
+const { findUserByGoogleEmail, createUser } = require('../model/userModel');
 
 const router = express.Router();
 
@@ -8,17 +9,30 @@ router.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUnin
 router.use(passport.initialize());
 router.use(passport.session());
 
-// router.get('/', (req, res) => {
-//   res.send('<a href="/auth/google"> Authentication with Google</a>');
-// });
-
 router.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
 
 router.get('/google/callback', passport.authenticate('google', {
   failureRedirect: '/google/failure' // Redirect to login page if authentication fails
-}), (req, res) => {
-  res.redirect('/google/callback'); // Redirect upon successful authentication
+}), async (req, res) => {
+  // Check if the user is already authenticated
+  if (req.isAuthenticated()) {
+    res.redirect('/'); // Redirect to the home page or wherever you want
+    return;
+  }
+
+  // If not authenticated, check if the user exists in the database
+  const user = await findUserByGoogleEmail(req.user.emails[0].value);
+  if (!user) {
+    // User doesn't exist, create a new user
+    const newUser = {
+      firstName: req.user.name.givenName,
+      lastName: req.user.name.familyName,
+      email: req.user.emails[0].value
+    };
+    await createUser(newUser);
+  }
+
+  res.redirect('/'); // Redirect upon successful authentication
 });
 
-
-module.exports = router; 
+module.exports = router;
